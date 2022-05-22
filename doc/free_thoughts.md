721f244d0b2bf74028cab846c94e0757c80d76a4
chris <christhedevel@gmail.com>
2022-05-22 20:32:09 +0200

    finish first version of library

--- doc/free_thoughts.md
+++ doc/free_thoughts.md
@@ -1,11 +1,30 @@
 # Flow of thoughts
 
-this document may or may not contain anything of relevance to the code as it looks right now. It serves mainly as a way for me to write down any thoughs I have on the project. It can be seen as my attempt to structure my thoughts a bit more by putting them down into ascii :D.
+this document may or may not contain anything of relevance to the code as it looks right now. It serves mainly as a way for me to write down any thoughts I have on the project. It can be seen as my attempt to structure my thoughts a bit more by putting them down into ascii :D. 
+
+# 22 May 2022
+
+what parameters should we have for each item:
+- difficulty
+- memory strength
+
+Comparing with algorithms like the one used in anki a good ratio between intervals (how large the next interval in comparison to the current) should be around 1.5.
+
+Periodically to evaluate the current parameters one can calculate the actual recall rate and compare it to the targeted recall rate. How much they differ can then be used to update the parameters.
+
+Should the parameters be updated using user evaluation or should they be updated by comparing wanted/actual recall rate? Can the two approaches be combined? They can by allowing the user reviews to affect the difficulty and the wanted/actual comparison to affect the memory strength change.
+
+How do we want to handle when the user forgets an item?
+- The item is made into a new item again.
+- The item is still scheduled in the future but not
+- we don't, we allow the library users to design their own way to handle it. 
+
+If we choose the third option then we will always be assuming that the recall events we're handling are always successful
 
 # 21 May 2022
 There are many features I want in a spaced repetition algorithm. It should be content agnostic. This means that the item being reviewed should be able to take any form with the only common piece of data between the items being that related to scheduling.
 
-A note on terminology. since I want a system that is not only designed around flashcards I instead refer to the item being continiously reviewed as simply that, an 'item'. I might every now and then slip up from this choice of words though and in those cases simply interpret me as meaning 'item'.
+A note on terminology. since I want a system that is not only designed around flashcards I instead refer to the item being continuously reviewed as simply that, an 'item'. I might every now and then slip up from this choice of words though and in those cases simply interpret me as meaning 'item'.
 
 
 - It seems that the modeling of human memory curve (probability of recalling an item) can be done using a exponential function such as the one below (assuming a positive value of f). 
@@ -18,13 +37,13 @@ $$ t = \frac{P}{-f} $$
 
 some good choices of parameters. (sign after parameter name signifies whether it increases the rate f or if it decreases it):
   - difficulty ++
-  - memory strenght --
-  - user evaluation score --/++ (might increase difficulty or memory strenght instead of acting as a new parameter).
+  - memory strength --
+  - user evaluation score --/++ (might increase difficulty or memory strength instead of acting as a new parameter).
 
-I do not want a timer that automatically fails a reviewing event. Some items could contain a promt that is is not supposed to be answered only a few seconds. Instead the user should be presented with how long their previous revision took and themselves make the judgement on their performance (using the 'user evalutation score').
+I do not want a timer that automatically fails a reviewing event. Some items could contain a promt that is is not supposed to be answered only a few seconds. Instead the user should be presented with how long their previous revision took and themselves make the judgement on their performance (using the 'user evaluation score').
 
 ### How should we set/update the values of our parameters?
-A problem is that we as the programer initially will choose very arbitrary parameters for all these values and how they should be changed over time. By allowing the user to also evaluate the performance of the algorithm continiously we can update these parameters to take better default values. 
+A problem is that we as the programmer initially will choose very arbitrary parameters for all these values and how they should be changed over time. By allowing the user to also evaluate the performance of the algorithm continuously we can update these parameters to take better default values. 
 
 It is very important that data is collected in some way such that the result of some parameter choice can be measured.
 
@@ -32,6 +51,6 @@ By having a system with such design I can initially make an educated guess on wh
 
 ### Handle over and under reviewing
 
-when choosing items to review, we should prioritize younger items first. The time intervall between revission events for such items are shorter and therefore missing a day or two means a more dramatic change in recall probability than for a more mature item.
+when choosing items to review, we should prioritize younger items first. The time interval between revision events for such items are shorter and therefore missing a day or two means a more dramatic change in recall probability than for a more mature item.
 
-We therefore when choosing items we want to compute how much they are overdue (or underdue) and relate that to their intervall. We then prioritizes items were that ratio is larger.
+We therefore when choosing items we want to compute how much they are overdue (or underdue) and relate that to their interval. We then prioritizes items were that ratio is larger.
---
+++ src/lib.rs
@@ -0,0 +1,136 @@
+//! This is a small library that defines a SM2 inspired spaced repetition algorithm. It tries to
+//! assume as little as possible about your items. It also tries to include some self adjusting
+//! behaviour such that we model the forgetting curve as well as possible. We also include some
+//! randomness such that we decouple items that were created together and allow their review events
+//! to spread out in time.
+#![warn(missing_docs)]
+#![warn(rustdoc::missing_crate_level_docs)]
+
+// stdlib imports
+use std::f64::consts::E;
+
+// external crate imports
+use rand::Rng;
+
+/// User evaluation of review event. Did it feel too hard, just difficult enough or too easy?
+pub enum UserReview {
+    /// The forgetting curve decreases faster than expected, increase difficulty of item
+    TooHard, 
+    /// The forgetting curve decreases just enough,
+    JustEnough, 
+    /// The forgetting curve decreases slower than expected, decrease difficulty of item
+    TooEasy, 
+}
+
+/// computes the number of days between the current review event and the next given a wanted recall
+/// probability. Expects a positive forgetting rate. Is not used directly can but is exposed
+/// anyway.
+#[inline]
+pub fn compute_intervall(forgetting_rate: f64, probability: f64) -> u32 {
+    assert!(forgetting_rate.is_sign_positive());
+    assert!(probability < 1.0);
+
+    let n_days_f = probability.log(E) / (-forgetting_rate);
+    n_days_f as u32
+}
+
+/// Struct containing item specific data related to it's scheduling. 
+pub struct SchedulingData {
+    intervall: u32,
+    difficulty: f64,
+    memory_strength: f64,
+    adjusting_factor: f64,
+    times_reviewed: u32,
+    times_recalled: u32,
+}
+
+/// struct containing various parameters used to update the scheduling data of an item
+#[derive(Clone, Debug)]
+pub struct UpdateParameters {
+    /// the factor (in percent) that the difficulty is increased/decreased if the user finds the
+    /// item to hard/easy
+    pub difficulty_change_factor: f64,
+    /// the faactor (in percent) that the memory_strength is increased when reviewing an item
+    pub memory_strength_change_factor: f64,
+}
+
+
+impl Default for SchedulingData {
+    fn default() -> Self {
+        // Here we want the initial ratio between the difficulty and the memory strength to be
+        // around -ln(0.9) =approx 0.1 (this results in the first interval being around 1 day)
+        // I therefore simply set the difficulty to that value, then scale both it and the memory
+        // strength by 100
+        SchedulingData { intervall: 1, difficulty: 10.0, memory_strength: 100.0, adjusting_factor: 1.0, times_reviewed: 0, times_recalled: 0 }
+    }
+}
+
+impl Default for UpdateParameters {
+    fn default() -> Self {
+        Self {
+            difficulty_change_factor: 1.1,
+            memory_strength_change_factor: 1.60,
+        }
+
+    }
+}
+
+/// main scheduling function. Takes the scheduling data of an item, and the result of the review
+/// event and computes the next intervall + changes to the item parameters.
+pub fn schedule(item_data: SchedulingData, user_review: UserReview, update_parameters: UpdateParameters, probability: f64) -> SchedulingData {
+    // The value of f will be the quotient difficulty/memory_strength.
+    // If we want to the ratio between the new and old interval to be A then that formes the
+    // following equation: t2 = A * t1. Which if expanded becomes:  A * ln(P)/-f1 = ln(P)/-f2. Out
+    // of this we then get the equation f1 * 1/A = f2 which can then be used to calculate the new
+    // value of f. Since f is a quotient we calculate this by first multiplying our wanted ratio
+    // with the memory_strength, update the difficulty with the user review and then compute the
+    // quotient.
+
+    // old data
+    let SchedulingData { intervall: _, difficulty, memory_strength, adjusting_factor, times_reviewed, times_recalled } = item_data;
+    
+    let new_difficulty = match user_review {
+        UserReview::TooHard => difficulty * update_parameters.difficulty_change_factor,
+        UserReview::JustEnough => difficulty,
+        UserReview::TooEasy => difficulty * (2.0  - update_parameters.difficulty_change_factor),
+    };
+
+    let new_memory_strength = memory_strength * update_parameters.memory_strength_change_factor;
+    let new_forgetting_rate = (1.0 / adjusting_factor) * (difficulty / memory_strength);
+    let next_interval_no_random = compute_intervall(new_forgetting_rate, probability);
+    
+    // we then want to introduce some noise in the interval
+    let mut rng = rand::thread_rng();
+    let random_range = next_interval_no_random / 10;
+    let random_change = rng.gen_range(0..random_range*2) - random_range;
+    let next_interval = next_interval_no_random + random_change;
+
+    SchedulingData {
+        intervall: next_interval,
+        difficulty: new_difficulty,
+        memory_strength: new_memory_strength,
+        adjusting_factor,
+        times_reviewed: times_reviewed + 1,
+        times_recalled: times_recalled + 1,
+    }
+}
+
+/// Computes how the ratio between review intervalls should be scaled to more accurately
+/// align with the true forgetting curve. Computed as explained [here](https://docs.ankiweb.net/deck-options.html#interval-modifier)
+pub fn update_adjusting_factor(item_data: SchedulingData, target_probability: f64) -> SchedulingData { 
+    let SchedulingData { intervall, difficulty, memory_strength, adjusting_factor: _, times_reviewed, times_recalled } = item_data;
+
+    // the actual recall probability for this item
+    let actual_probability = times_recalled as f64 / times_reviewed as f64;
+
+    let new_adjusting_factor = target_probability.log(E) / actual_probability.log(E);
+
+    SchedulingData {
+        intervall,
+        difficulty,
+        memory_strength,
+        adjusting_factor: new_adjusting_factor,
+        times_reviewed,
+        times_recalled
+    }
+}

4908296d6a0fc34617989a5a33f546d2b0e7b84d
christhedevel <christhedevel@gmail.com>
2022-06-20 14:32:42 +0200

    update visibility of item struct

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@
 #![warn(missing_docs)]
 #![warn(rustdoc::missing_crate_level_docs)]
 
-
 // stdlib imports
 use std::f32::consts::E;
 
@@ -16,11 +15,11 @@ use rand::Rng;
 /// User evaluation of review event. Did it feel too hard, just difficult enough or too easy?
 pub enum UserReview {
     /// The forgetting curve decreases faster than expected, increase difficulty of item
-    TooHard, 
+    TooHard,
     /// The forgetting curve decreases just enough,
-    JustEnough, 
+    JustEnough,
     /// The forgetting curve decreases slower than expected, decrease difficulty of item
-    TooEasy, 
+    TooEasy,
 }
 
 /// computes the number of days between the current review event and the next given a wanted recall
@@ -35,14 +34,14 @@ pub fn compute_interval(forgetting_rate: f32, probability: f32) -> i32 {
     n_days_f as i32
 }
 
-/// Struct containing item specific data related to it's scheduling. 
+/// Struct containing item specific data related to it's scheduling.
 pub struct SchedulingData {
-    interval: i32,
-    difficulty: f32,
-    memory_strength: f32,
-    adjusting_factor: f32,
-    times_reviewed: i32,
-    times_recalled: i32,
+    pub interval: i32,
+    pub difficulty: f32,
+    pub memory_strength: f32,
+    pub adjusting_factor: f32,
+    pub times_reviewed: i32,
+    pub times_recalled: i32,
 }
 
 /// struct containing various parameters used to update the scheduling data of an item
@@ -55,14 +54,20 @@ pub struct UpdateParameters {
     pub memory_strength_change_factor: f32,
 }
 
-
 impl Default for SchedulingData {
     fn default() -> Self {
         // Here we want the initial ratio between the difficulty and the memory strength to be
         // around -ln(0.9) =approx 0.1 (this results in the first interval being around 1 day)
         // I therefore simply set the difficulty to that value, then scale both it and the memory
         // strength by 100
-        SchedulingData { interval: 1, difficulty: 10.0, memory_strength: 100.0, adjusting_factor: 1.0, times_reviewed: 0, times_recalled: 0 }
+        SchedulingData {
+            interval: 1,
+            difficulty: 10.0,
+            memory_strength: 100.0,
+            adjusting_factor: 1.0,
+            times_reviewed: 0,
+            times_recalled: 0,
+        }
     }
 }
 
@@ -72,13 +77,17 @@ impl Default for UpdateParameters {
             difficulty_change_factor: 1.1,
             memory_strength_change_factor: 1.60,
         }
-
     }
 }
 
 /// main scheduling function. Takes the scheduling data of an item, and the result of the review
 /// event and computes the next interval + changes to the item parameters.
-pub fn schedule(item_data: SchedulingData, user_review: UserReview, update_parameters: UpdateParameters, probability: f32) -> SchedulingData {
+pub fn schedule(
+    item_data: SchedulingData,
+    user_review: UserReview,
+    update_parameters: UpdateParameters,
+    probability: f32,
+) -> SchedulingData {
     // The value of f will be the quotient difficulty/memory_strength.
     // If we want to the ratio between the new and old interval to be A then that formes the
     // following equation: t2 = A * t1. Which if expanded becomes:  A * ln(P)/-f1 = ln(P)/-f2. Out
@@ -88,22 +97,29 @@ pub fn schedule(item_data: SchedulingData, user_review: UserReview, update_param
     // quotient.
 
     // old data
-    let SchedulingData { interval: _, difficulty, memory_strength, adjusting_factor, times_reviewed, times_recalled } = item_data;
-    
+    let SchedulingData {
+        interval: _,
+        difficulty,
+        memory_strength,
+        adjusting_factor,
+        times_reviewed,
+        times_recalled,
+    } = item_data;
+
     let new_difficulty = match user_review {
         UserReview::TooHard => difficulty * update_parameters.difficulty_change_factor,
         UserReview::JustEnough => difficulty,
-        UserReview::TooEasy => difficulty * (2.0  - update_parameters.difficulty_change_factor),
+        UserReview::TooEasy => difficulty * (2.0 - update_parameters.difficulty_change_factor),
     };
 
     let new_memory_strength = memory_strength * update_parameters.memory_strength_change_factor;
     let new_forgetting_rate = (1.0 / adjusting_factor) * (difficulty / memory_strength);
     let next_interval_no_random = compute_interval(new_forgetting_rate, probability);
-    
+
     // we then want to introduce some noise in the interval
     let mut rng = rand::thread_rng();
     let random_range = next_interval_no_random / 10;
-    let random_change = rng.gen_range(0..random_range*2) - random_range;
+    let random_change = rng.gen_range(0..random_range * 2) - random_range;
     let next_interval = next_interval_no_random + random_change;
 
     SchedulingData {
@@ -118,8 +134,18 @@ pub fn schedule(item_data: SchedulingData, user_review: UserReview, update_param
 
 /// Computes how the ratio between review intervals should be scaled to more accurately
 /// align with the true forgetting curve. Computed as explained [here](https://docs.ankiweb.net/deck-options.html#interval-modifier)
-pub fn update_adjusting_factor(item_data: SchedulingData, target_probability: f32) -> SchedulingData { 
-    let SchedulingData { interval, difficulty, memory_strength, adjusting_factor: _, times_reviewed, times_recalled } = item_data;
+pub fn update_adjusting_factor(
+    item_data: SchedulingData,
+    target_probability: f32,
+) -> SchedulingData {
+    let SchedulingData {
+        interval,
+        difficulty,
+        memory_strength,
+        adjusting_factor: _,
+        times_reviewed,
+        times_recalled,
+    } = item_data;
 
     // the actual recall probability for this item
     let actual_probability = times_recalled as f32 / times_reviewed as f32;
@@ -132,6 +158,6 @@ pub fn update_adjusting_factor(item_data: SchedulingData, target_probability: f3
         memory_strength,
         adjusting_factor: new_adjusting_factor,
         times_reviewed,
-        times_recalled
+        times_recalled,
     }
 }
